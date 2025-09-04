867f1a68b5a6502c690b25caa9a0588e131fad47
John Lewis <github@jlewis.sh>
2025-09-04 12:06:58 -0700

    fix: cut unnecessary gnome packages

--- config/modules/nixos/graphical.nix
+++ config/modules/nixos/graphical.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: let
+{ lib, config, pkgs, ... }: let
   cfg = config.graphical;
 in {
   options = {
@@ -12,6 +12,24 @@ in {
 
     services.xserver.desktopManager.gnome.enable = true;
     services.xserver.displayManager.gdm.enable = true;
+
+    environment.gnome.excludePackages = (with pkgs; [
+      gnome-photos
+      gnome-tour
+    ]) ++ (with pkgs; [
+      gnome-music
+      # gedit # text editor
+      epiphany # web browser
+      geary # email reader
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      yelp # Help view
+      gnome-contacts
+      gnome-initial-setup
+      gnome-calendar
+    ]);
 
     networking.networkmanager.enable = true;
   };
