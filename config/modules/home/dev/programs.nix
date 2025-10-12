b927e875621ba29fabaed366a694f57ca78dec8e
John Lewis <github@jlewis.sh>
2025-10-12 13:43:03 -0700

    feat: add rest of the dev programs to path

--- config/modules/home/dev/programs.nix
+++ config/modules/home/dev/programs.nix
@@ -1,4 +1,42 @@
-{ ... }: {
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    # basic shell utils
+    just file fzf
+
+    # vcs
+    gitoxide
+
+    # replace coreutils with rust rewrite
+    uutils-coreutils-noprefix
+
+    # archives
+    unzip gzip 
+
+    # http & friends
+    curl wget jq
+
+    # extra nix helpers
+    comma nix-tree
+
+    # networking
+    nmap inetutils cfspeedtest
+
+    # other utilities
+    tio # serial device tool
+    gurk-rs # signal client
+    pass gnome-keyring
+
+    # asahi vm stuff
+    distrobox
+
+    # misc
+    protonvpn-gui
+    sl # steam locomotive
+    typer # typing test
+    fastfetch # rip neofetch :(
+    spotify-player
+  ];
+  
   # replacement for `cat`
   programs.bat.enable = true;
 
