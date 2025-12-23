a43f2f355869a9cda960df0522faa5e16da40cd0
John Lewis <github@jlewis.sh>
2025-12-23 13:24:45 -0800

    fix: update inputs and remove asahi mesa workaround

--- config/modules/nixos/asahi.nix
+++ config/modules/nixos/asahi.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }: let
+{ lib, config, ... }: let
   cfg = config.hardware.asahi-hardware;
 in {
   options = {
@@ -6,15 +6,6 @@ in {
   };
 
   config = {
-    hardware.graphics.package =
-      # Workaround for Mesa 25.3.0/.1 regression
-      # https://github.com/nix-community/nixos-apple-silicon/issues/380
-      assert pkgs.mesa.version == "25.3.1";
-      (import (fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/c5ae371f1a6a7fd27823bc500d9390b38c05fa55.tar.gz";
-        sha256 = "sha256-4PqRErxfe+2toFJFgcRKZ0UI9NSIOJa+7RXVtBhy4KE=";
-      }) { localSystem = pkgs.stdenv.hostPlatform; }).mesa;
-    
     hardware.asahi = {
       # switch asahi to default off
       enable = cfg.enable;
