dfdad20d0bf8bd8ae82ba1dc01b1e60f71758270
John Lewis <github@jlewis.sh>
2026-04-06 16:49:15 -0700

    fix: disable openclaw

--- config/modules/home/dev/ai.nix
+++ config/modules/home/dev/ai.nix
@@ -7,42 +7,42 @@
     signal-cli # for openclaw channel
   ];
 
-  programs.openclaw = {
-    enable = true;
-    documents = ../../../../openclaw-documents;
+  # programs.openclaw = {
+  #   enable = true;
+  #   documents = ../../../../openclaw-documents;
 
-    config = {
-      auth = {
-        profiles = {
-          "anthropic:default" = {
-            provider = "anthropic";
-            mode = "token";
-          };
-        };
-      };
-      gateway = {
-        port = 18789;
-        mode = "local";
-        bind = "loopback";
-        auth = {
-          mode = "token";
-          token = "32f2bed0dc13500b27088bc7237bd43c5f3c11cf058541affcba7d9e31112955";
-        };
-        tailscale = {
-          mode = "off";
-          resetOnExit = false;
-        };
-        # controlUi.root = "/home/jlewis/.openclaw/control-ui";
-      };
-      agents = {
-        defaults = {
-          model = { primary = "anthropic/claude-sonnet-4-6"; };
-          models = {
-            "anthropic/claude-sonnet-4-6" = { alias = "Sonnet"; };
-            "anthropic/claude-opus-4-6" = { alias = "Opus"; };
-          };
-        };
-      };
-    };
-  };
+  #   config = {
+  #     auth = {
+  #       profiles = {
+  #         "anthropic:default" = {
+  #           provider = "anthropic";
+  #           mode = "token";
+  #         };
+  #       };
+  #     };
+  #     gateway = {
+  #       port = 18789;
+  #       mode = "local";
+  #       bind = "loopback";
+  #       auth = {
+  #         mode = "token";
+  #         token = "32f2bed0dc13500b27088bc7237bd43c5f3c11cf058541affcba7d9e31112955";
+  #       };
+  #       tailscale = {
+  #         mode = "off";
+  #         resetOnExit = false;
+  #       };
+  #       # controlUi.root = "/home/jlewis/.openclaw/control-ui";
+  #     };
+  #     agents = {
+  #       defaults = {
+  #         model = { primary = "anthropic/claude-sonnet-4-6"; };
+  #         models = {
+  #           "anthropic/claude-sonnet-4-6" = { alias = "Sonnet"; };
+  #           "anthropic/claude-opus-4-6" = { alias = "Opus"; };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 }
