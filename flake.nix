749c16374deebc6bc312a745e2beab108a835cd7
John Lewis <github@jlewis.sh>
2026-04-21 16:09:30 -0700

    chore: remove dead inputs

--- flake.nix
+++ flake.nix
@@ -11,8 +11,6 @@
       url = "github:alacritty/alacritty-theme";
       flake = false;
     };
-    # nix-openclaw.url = "github:openclaw/nix-openclaw";
-    # bluehood.url = "github:johnbchron/bluehood-flake";
     iosevka-pin.url = "github:NixOS/nixpkgs?rev=77a52192a7502c2385027651c81d269cb106dbde";
 
     jj-watch.url = "github:johnbchron/jj-watch";
