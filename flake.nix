d4ce05078fff8d05b83edde2e4c41b193cd693b9
John Lewis <github@jlewis.sh>
2026-04-24 10:53:58 -0700

    chore: update flake inputs, with mesa workaround

--- flake.nix
+++ flake.nix
@@ -16,7 +16,8 @@
     jj-watch.url = "github:johnbchron/jj-watch";
 
     apple-silicon-support = {
-      url = "github:nix-community/nixos-apple-silicon";
+      # url = "github:nix-community/nixos-apple-silicon";
+      url = "github:nix-community/nixos-apple-silicon/mesa-26.0.5";
     };
   };
 
