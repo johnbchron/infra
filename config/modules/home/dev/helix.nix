7294ba35adf056fb6490f6ab65a500ed22602b4d
John Lewis <github@jlewis.sh>
2026-07-01 09:35:24 -0500

    fix: correct whitespace in tinymist config

--- config/modules/home/dev/helix.nix
+++ config/modules/home/dev/helix.nix
@@ -107,7 +107,7 @@
         tinymist = {
           config = {
             preview.background.enabled = true;
-            preview.background.args = ["--data-plane-host=127.0.0.1:0" "--invert-colors=never""--open"];
+            preview.background.args = ["--data-plane-host=127.0.0.1:0" "--invert-colors=never" "--open"];
           };
         };
 
