355658c2c0694425c7aa65094b4eb398ff6b90a2
John Lewis <github@jlewis.sh>
2026-09-16 12:00:08 -0500

    feat: add crush

--- config/modules/common/graphical.nix
+++ config/modules/common/graphical.nix
@@ -11,7 +11,7 @@ in {
     nixpkgs = {
       config = {
         allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-          "obsidian" "claude-code" "steam-unwrapped"
+          "obsidian" "claude-code" "steam-unwrapped" "crush"
         ];
       };
 
--- config/modules/home/dev/ai.nix
+++ config/modules/home/dev/ai.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    claude-code
+    claude-code crush
   ];
 }
